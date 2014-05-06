d7a11f0367464989e50f46118848668500c42924
Mateusz Aniolek <matty201@gmail.com>
2014-05-06 13:09:40 +0200

    Revert "ExporterAbstract documentation"
    This reverts commit 0da5fd026f5f10f4e90b28b79dba783da0430134.

--- src/ExporterAbstract.php
+++ src/ExporterAbstract.php
@@ -3,49 +3,22 @@
 abstract class ExporterAbstract {
 
     /**
-     *  Contans std object with content properties:
-     *  - contentType
-     *  - contentSize
-     *  - filename
+     *
      * @var StdClass
      *  
      */
     protected $properties;
     
-    /**
-     *  Contains file path 
-     * @var String
-     *  
-     */
     protected $outputPath = null;
     
-    /**
-     *  Abstract init, inside should be created stdClass object 
-     * @param array $data set of data to export
-     * @param type $mongo if we are going to use mongo db set this var to true
-     */
-    abstract function init(array $data, $mongo = false);
+    abstract function init(array $data, bool $mongo = false);
     
-    
-    /**
-     *  It sets header row for output data
-     * @param array $headers data with header
-     */
     abstract function setHeaders(array $headers);
     
-    /**
-     * @param string $filename Name of file, override with default set value
-     */
     abstract function export($filename);
     
-    /**
-     * It returns content that should be exported
-     */
     abstract function getContent();
     
-    /**
-     * Download function. Sets a few header, and print content
-     */
     function download()
     {
         header('Content-Type: '. $this->properties->contentType);
@@ -56,10 +29,6 @@ abstract class ExporterAbstract {
         exit;        
     }
     
-    /**
-     *  Setter
-     * @param String $path
-     */
     function setOutputPath($path)
     {
         $this->outputPath = $path;
