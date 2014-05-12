01de7cd4e409ed6d7d860606f36baba5b8e5ad4f
Mateusz Aniolek <matty201@gmail.com>
2014-05-12 11:51:55 +0200

    Structual fix

--- src/Xml.php
+++ src/Xml.php
@@ -24,6 +24,12 @@ class Xml extends ExporterAbstract {
      */
     public $keysAsHeaders;
         
+    /**
+     * Sets data and create properties object
+     * @param array $data
+     * @param boolean $keysAsHeaders
+     * @throws ExporterException
+     */
     public function init(array $data, $keysAsHeaders = true) 
     {
         if(is_null($data)){
@@ -31,26 +37,32 @@ class Xml extends ExporterAbstract {
         }
       
         $this->keysAsHeaders = $keysAsHeaders;
-        
-        
+                
         $this->data = $data;
                 
         $this->properties = new \stdClass();
         $this->properties->contentSize = null;
-        $this->properties->contentType = 'Xml';
+        $this->properties->contentType = 'application/xml';
         $this->properties->filename = null;
                 
     }
     
-
+    /**
+     * Returns generated XML as string
+     * @return string
+     */
     public function getContent() 
     {
         return $this->content->asXML();
     }
-
+    
+    /**
+     * Implementation of exportData function
+     * @param string $filename
+     * @throws ExporterException
+     */
     public function exportData($filename = 'export_file.xml') 
     {
-        
         if($this->keysAsHeaders === false && is_null($this->headers)){
             throw new ExporterException('Headers cannot be empty if var keysAsHeaders was given in init function');
         }
@@ -62,6 +74,10 @@ class Xml extends ExporterAbstract {
         
     }
     
+    /**
+     * Generates SimpleXMLElement object from data which was set in init()
+     * @param string $filename
+     */
     public function initXml()
     {
         $xmlstr = '<?xml version="1.0" encoding="utf-8"?><root></root>';
@@ -73,14 +89,10 @@ class Xml extends ExporterAbstract {
             
             foreach($items as $key => $item){
                 
-                if($this->keysAsHeaders){
-            
-                    $parent->addChild($key, $item);
-            
-                } else {
-                    
-                    $parent->addChild($this->headers[$key], $item);
-                    
+                if($this->keysAsHeaders){            
+                    $parent->addChild($key, $item);            
+                } else {                    
+                    $parent->addChild($this->headers[$key], $item);                    
                 }
             
             }
@@ -91,6 +103,10 @@ class Xml extends ExporterAbstract {
                 
     }
     
+    /**
+     * Saving xml file 
+     * @param string $filename
+     */
     public function saveXml($filename)
     {
         return $this->content->asXML($filename);
