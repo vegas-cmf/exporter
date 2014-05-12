3fb5a6d47339de00dfcd6944b2c76bea6e0a2871
Mateusz Aniolek <matty201@gmail.com>
2014-05-12 13:43:12 +0200

    corrected generating of csv, tests

--- src/Csv.php
+++ src/Csv.php
@@ -12,6 +12,19 @@ class Csv extends ExporterAbstract {
      */
     private $data;
     
+    
+    /**
+     * Separator for csv values
+     * @var char 
+     */
+    private $value_separator;
+    
+    /**
+     * Separator for new lines in csv
+     * @var char 
+     */
+    private $nl_separator;
+    
     /**
      * Variable contains PHPExcel object 
      * @var PHPExcel 
@@ -24,13 +37,16 @@ class Csv extends ExporterAbstract {
      */
     private $keysAsHeaders;
         
+    
     /**
-     * Sets data and create properties object
+     * Sets fundamental properties of class (data, separators, headers)
      * @param array $data
      * @param boolean $keysAsHeaders
+     * @param char $value_separator
+     * @param char $nl_separator
      * @throws ExporterException
      */
-    public function init(array $data, $keysAsHeaders = true) 
+    public function init(array $data, $keysAsHeaders = true, $value_separator = ',', $nl_separator = ';') 
     {
         if(is_null($data)){
             throw new ExporterException('Data cannot be empty');
@@ -39,16 +55,19 @@ class Csv extends ExporterAbstract {
         $this->keysAsHeaders = $keysAsHeaders;
         
         $this->data = $data;
-                
+        
+        $this->value_separator = $value_separator;
+        $this->nl_separator = $nl_separator;
+                        
         $this->properties = new \stdClass();
         $this->properties->contentSize = null;
-        $this->properties->contentType = 'application/vnd.ms-excel';
+        $this->properties->contentType = 'text/csv';
         $this->properties->filename = null;
                 
     }
     
     /**
-     * Returns instance of PHPExcel
+     * Returns content of generated values
      * @return PHPExcel
      */
     public function getContent() 
@@ -58,7 +77,7 @@ class Csv extends ExporterAbstract {
     
     /**
      * Returns state of headers
-     * @return PHPExcel
+     * @return boolean
      */
     public function getKeysAsHeaders() 
     {
@@ -70,7 +89,7 @@ class Csv extends ExporterAbstract {
      * @param string $filename
      * @throws ExporterException
      */
-    public function exportData($filename = "export_file.xls") 
+    public function exportData($filename = "export_file.csv") 
     {        
         if($this->keysAsHeaders === false && is_null($this->headers)){
             throw new ExporterException("Headers cannot be empty if var keysAsHeaders was given in init function");
@@ -78,56 +97,50 @@ class Csv extends ExporterAbstract {
         
         $this->properties->filename = $filename;
         
-        $this->content = new \PHPExcel();
-        $this->content->setActiveSheetIndex(0);
-        
-        $this->initExcelValues();
-        $this->saveExcel($filename);
+        $this->initCsvValues();
+        $this->saveCsv($filename);
         
     }
     
     /**
-     * Generates worksheet for xls file. In addition, it saves temporary our 
-     * file to get content length. It's necessary to download file.
+     * Generates values for csv file. Also it sets contentSize variable to 
+     * possibility downloading file when it needs.
      */
-    public function initExcelValues()
-    {        
-        $i = 1;
-        $sheet = $this->content->getActiveSheet();
-
-        if(!$this->keysAsHeaders){
-            array_unshift($this->data, $this->headers);
+    public function initCsvValues()
+    {      
+        $csv = "";
+        
+        if($this->keysAsHeaders === true){
+            foreach($this->headers as $key){
+                $csv .= $key . $this->value_separator;
+            }
+            
+            $csv = substr($csv, 0, -1) . $this->nl_separator;
         }
         
         foreach($this->data as $item){
             
-            $column = 'A';
             foreach($item as $value){
-                $sheet->getCell($column.$i)->setValue($value);
-                $column++;
+                $csv .= $value . $this->value_separator;
             }
             
-            $i++;
+            $csv = substr($csv, 0, -1) . $this->nl_separator;
             
         }
-        $temporary_file = $this->outputPath . "test.xls";
-        $writer = new \PHPExcel_Writer_Excel2007($this->content);
-        $writer->save($temporary_file);
         
-        $this->properties->contentSize = filesize($temporary_file);
-        unlink($temporary_file);
+        $this->content = $csv;
+        
+        $this->properties->contentSize = strlen($this->content);
                 
     }
     
     /**
-     * Saving excel file
+     * Saving values to .csv file as name given in filename variable.
      * @param string $filename
      */
-    public function saveExcel($filename)
+    public function saveCsv($filename)
     {        
-        $writer = new \PHPExcel_Writer_Excel2007($this->content);
-        $writer->save($this->outputPath . $filename);
-        
+        file_put_contents($this->outputPath . $filename, $this->content);        
     }
     
 
